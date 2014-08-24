274aac49444385a254c8fa29708f73e94ddcd423
Christopher Poole <mail@christopherpoole.net>
2014-08-25 09:42:35 +1000

    Fixed a couple of typos.

--- setup.cmake
+++ setup.cmake
@@ -6,11 +6,13 @@
 #
 # --- Build Setup and Options ---
 
-set(SERVANT "Algorimth" CACHE STRING "The name of the algorithm")
+set(SERVANT "Algorithm" CACHE STRING "The name of the algorithm")
 set(VERSION "0.0.1" CACHE STRING "The version of the algorithm")
 set(DCF_DIRECTORY "D:/VMSOS/DCF" CACHE PATH "The path to the Distributed Calculation Framework installation")
 
 option(OVERWRITE "Overwrite any current installation with the same version" OFF)
+option(REGISTER_SERVANT "Register the servant during installation" ON)
+option(REGISTER_ALGORITHM "Register the algorithm during installation" ON)
 option(FORCE_AGENT_RELOAD "Instruct agents to reload the configuration (requires curl)" OFF)
 option(COPY_CALCULATION_OPTIONS "Copy the Calculation Options schema and files." OFF)
 option(COPY_MATERIAL_TABLE "Copy the Physical Material Table for this algorithm. In the RT Administrator, Clinical Data -> Physical Materials -> Import from DCF, to populate the ARIA database." OFF)
@@ -27,7 +29,7 @@ if (EXISTS ${CMAKE_INSTALL_PREFIX})
     endif()
 endif()
 
-# Append 'Servant' to the algorith name
+# Append 'Servant' to the algorithm name
 set(SERVANT "${SERVANT}Servant")
 
 # Setup the project.
